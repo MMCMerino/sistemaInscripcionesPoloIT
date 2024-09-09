efe737fd862778863dff42517f407430accdd198
Heliana <86692848+HelianaARivas@users.noreply.github.com>
2024-09-09 13:06:44 -0300

    Update Administrador.java
    se agrego cosas

--- proyectoWebMaven/sistemaPoloit/src/main/java/Logica/Administrador.java
+++ proyectoWebMaven/sistemaPoloit/src/main/java/Logica/Administrador.java
@@ -1,21 +1,34 @@
 package logica;
 
 import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
-
+@Entity
 public class Administrador extends Persona{
     
     private String especializacion;
     private String cargo;
+    @OneToOne
+    private Usuario unUsuario;
     
     public Administrador() {
     }
 
-    public Administrador( String especializacion, String cargo, String nombre, String apellido, String correo, String telefono, Date fecha_nac) {
+    public Administrador( String especializacion, String cargo, Usuario unUsuario, String nombre, String apellido, String correo, String telefono, Date fecha_nac) {
         super(nombre, apellido, correo, telefono, fecha_nac);
         
         this.especializacion = especializacion;
         this.cargo = cargo;
+        this.unUsuario = unUsuario;
+    }
+    
+    public Usuario getUnUsuario() {
+        return unUsuario;
+    }
+
+    public void setUnUsuario(Usuario unUsuario) {
+        this.unUsuario = unUsuario;
     }
 
 
