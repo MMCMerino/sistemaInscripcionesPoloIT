5b7deb6759b428868ad3f36305d24719a036713c
MMCMerino <mariamartamerino@gmail.com>
2024-10-18 17:48:30 -0300

    validacion login final

--- proyectoWebMaven/sistemaPoloit/src/main/java/logica/Controladora.java
+++ proyectoWebMaven/sistemaPoloit/src/main/java/logica/Controladora.java
@@ -7,7 +7,6 @@ package logica;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import static logica.Usuario_.contrasenia;
 import persistencia.ControladoraPersistencia;
 
 /**
